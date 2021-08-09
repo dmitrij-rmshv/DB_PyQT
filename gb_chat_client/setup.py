441a7345aa28343f2148f72963386907bf51b4f9
Dmitrij <gdems@operamail.com>
2021-08-09 23:24:00 +0300

    wheel pkg done

---
+++ gb_chat_client/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name="math_package",
+      version="0.1",
+      description="Chat server",
+      author="Dmitriy Romashev",
+      author_email="romashev.dmitrij@gmail.com",
+      url="http://x995993v.beget.tech/",
+      packages=["src"]
+      )
